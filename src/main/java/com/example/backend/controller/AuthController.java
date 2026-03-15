c850c348b5daeddd833c5fc5cf7df5e547bed1a7
Utej <91786744+utej8553@users.noreply.github.com>
2026-03-15 17:06:36 +0530

    Update AuthController.java

--- src/main/java/com/example/backend/controller/AuthController.java
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigins(origin = "*");
+@CrossOrigin(origins = "*");
 public class AuthController {
 
     private final AuthService authService;
