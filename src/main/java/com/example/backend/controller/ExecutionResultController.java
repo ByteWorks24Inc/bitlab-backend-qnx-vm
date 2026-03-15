e8bff88fa319afb75a12433f2d5860d087ade35a
Utej <91786744+utej8553@users.noreply.github.com>
2026-03-15 16:20:10 +0530

    Add CrossOrigin annotation to ExecutionResultController

--- src/main/java/com/example/backend/controller/ExecutionResultController.java
+++ src/main/java/com/example/backend/controller/ExecutionResultController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/result")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ExecutionResultController {
 
     @PostMapping
